9d717780b73be48f854e3c8959cd9ef873ddccd5
NiyaziAki <11836469+NiyaziAki@users.noreply.github.com>
2018-09-16 19:23:47 +0300

    utf8 refactoring.

--- utils/encoding/utf8.js
+++ utils/encoding/utf8.js
@@ -1,47 +1,128 @@
 const isString = require("../validation/is-string");
 const isByteArray = require("../validation/is-byte-array");
 
-const encodeTwoBytes = charCode => {
-  const byte1 = (charCode >> 6) | 0b11000000;
-  const byte2 = (charCode & 0b00111111) | 0b10000000;
-
-  return [byte1, byte2];
+const getCharCode = chars => {
+  if (chars.length === 1) {
+    return chars.charCodeAt();
+  }
+  return chars.codePointAt();
 };
 
-const encodeThreeBytes = charCode => {
-  const byte1 = (charCode >> 12) | 0b11100000;
-  const byte2 = ((charCode >> 6) & 0b00111111) | 0b10000000;
-  const byte3 = (charCode & 0b00111111) | 0b10000000;
-
-  return [byte1, byte2, byte3];
-};
-
-const encodeFourBytes = charCode => {
+const encodeFourBytes = (byteArray, charCode) => {
+  /* 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx*/
   const byte1 = (charCode >> 18) | 0b11110000;
   const byte2 = ((charCode >> 12) & 0b00111111) | 0b10000000;
   const byte3 = ((charCode >> 6) & 0b00111111) | 0b10000000;
   const byte4 = (charCode & 0b00111111) | 0b10000000;
 
-  return [byte1, byte2, byte3, byte4];
+  byteArray.push(byte1);
+  byteArray.push(byte2);
+  byteArray.push(byte3);
+  byteArray.push(byte4);
 };
 
-const decodeTwoBytes = (byte1, byte2) =>
-  String.fromCharCode(((byte1 & 0b00011111) << 6) | (byte2 & 0b00111111));
+const encodeThreeBytes = (byteArray, charCode) => {
+  if (charCode < 0b10000000000000000) {
+    /* 1110xxxx 10xxxxxx 10xxxxxx*/
+    const byte1 = (charCode >> 12) | 0b11100000;
+    const byte2 = ((charCode >> 6) & 0b00111111) | 0b10000000;
+    const byte3 = (charCode & 0b00111111) | 0b10000000;
 
-const decodeThreeBytes = (byte1, byte2, byte3) =>
-  String.fromCharCode(
-    ((byte1 & 0b00001111) << 12) |
-      ((byte2 & 0b00111111) << 6) |
-      (byte3 & 0b00111111)
-  );
+    byteArray.push(byte1);
+    byteArray.push(byte2);
+    byteArray.push(byte3);
+  } else {
+    encodeFourBytes(byteArray, charCode);
+  }
+};
 
-const decodeFourBytes = (byte1, byte2, byte3, byte4) =>
-  String.fromCodePoint(
-    ((byte1 & 0b00000111) << 18) |
-      ((byte2 & 0b00111111) << 12) |
-      ((byte3 & 0b00111111) << 6) |
-      (byte4 & 0b00111111)
-  );
+const encodeTwoBytes = (byteArray, charCode) => {
+  if (charCode < 0b100000000000) {
+    /* 110xxxxx 10xxxxxx*/
+    const byte1 = (charCode >> 6) | 0b11000000;
+    const byte2 = (charCode & 0b00111111) | 0b10000000;
+
+    byteArray.push(byte1);
+    byteArray.push(byte2);
+  } else {
+    encodeThreeBytes(byteArray, charCode);
+  }
+};
+
+const encodeBytes = (byteArray, charCode) => {
+  if (charCode < 0b10000000) {
+    /* 0xxxxxxx */
+    byteArray.push(charCode);
+  } else {
+    encodeTwoBytes(byteArray, charCode);
+  }
+};
+
+const decodeFourBytes = (byteArray, index) => {
+  /* 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx*/
+  const byte1 = byteArray[index];
+  const byte2 = byteArray[index + 1];
+  const byte3 = byteArray[index + 2];
+  const byte4 = byteArray[index + 3];
+
+  return [
+    String.fromCodePoint(
+      ((byte1 & 0b00000111) << 18) |
+        ((byte2 & 0b00111111) << 12) |
+        ((byte3 & 0b00111111) << 6) |
+        (byte4 & 0b00111111)
+    ),
+    3
+  ];
+};
+
+const decodeThreeBytes = (byteArray, index) => {
+  const byte1 = byteArray[index];
+
+  if (byte1 > 0xdf && byte1 < 0xf0) {
+    /* 1110xxxx 10xxxxxx 10xxxxxx */
+    const byte2 = byteArray[index + 1];
+    const byte3 = byteArray[index + 2];
+
+    return [
+      String.fromCharCode(
+        ((byte1 & 0b00001111) << 12) |
+          ((byte2 & 0b00111111) << 6) |
+          (byte3 & 0b00111111)
+      ),
+      2
+    ];
+  }
+
+  return decodeFourBytes(byteArray, index);
+};
+
+const decodeTwoBytes = (byteArray, index) => {
+  const byte1 = byteArray[index];
+
+  if (byte1 > 0b10111111 && byte1 < 0b11100000) {
+    /* 110xxxxx 10xxxxxx */
+    const byte2 = byteArray[index + 1];
+
+    return [
+      String.fromCharCode(((byte1 & 0b00011111) << 6) | (byte2 & 0b00111111)),
+      1
+    ];
+  }
+
+  return decodeThreeBytes(byteArray, index);
+};
+
+const decodeBytes = (byteArray, index) => {
+  const byte1 = byteArray[index];
+
+  if (byte1 < 0b10000000) {
+    /* 0xxxxxxx*/
+    return [String.fromCharCode(byte1), 0];
+  }
+
+  return decodeTwoBytes(byteArray, index);
+};
 
 const encode = text => {
   if (!isString(text)) {
@@ -50,22 +131,9 @@ const encode = text => {
   const byteArray = [];
 
   for (const chars of text) {
-    const charCode =
-      chars.length === 1 ? chars.charCodeAt() : chars.codePointAt();
+    const charCode = getCharCode(chars);
 
-    if (charCode < 0b10000000) {
-      /* 0xxxxxxx */
-      byteArray.push(charCode);
-    } else if (charCode < 0b100000000000) {
-      /* 110xxxxx 10xxxxxx*/
-      byteArray.push(...encodeTwoBytes(charCode));
-    } else if (charCode < 0b10000000000000000) {
-      /* 1110xxxx 10xxxxxx 10xxxxxx*/
-      byteArray.push(...encodeThreeBytes(charCode));
-    } else {
-      /* 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx*/
-      byteArray.push(...encodeFourBytes(charCode));
-    }
+    encodeBytes(byteArray, charCode);
   }
 
   return new Uint8Array(byteArray);
@@ -78,33 +146,10 @@ const decode = byteArray => {
   let decodedString = "";
 
   for (let index = 0; index < byteArray.length; index++) {
-    const byte1 = byteArray[index];
+    const decodedBytes = decodeBytes(byteArray, index);
 
-    if (byte1 < 0b10000000) {
-      /* 0xxxxxxx*/
-      decodedString += String.fromCharCode(byte1);
-    } else if (byte1 > 0b10111111 && byte1 < 0b11100000) {
-      /* 110xxxxx 10xxxxxx */
-      decodedString += decodeTwoBytes(byte1, byteArray[index + 1]);
-      index += 1;
-    } else if (byte1 > 0xdf && byte1 < 0xf0) {
-      /* 1110xxxx 10xxxxxx 10xxxxxx */
-      decodedString += decodeThreeBytes(
-        byte1,
-        byteArray[index + 1],
-        byteArray[index + 2]
-      );
-      index += 2;
-    } else {
-      /* 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx*/
-      decodedString += decodeFourBytes(
-        byte1,
-        byteArray[index + 1],
-        byteArray[index + 2],
-        byteArray[index + 3]
-      );
-      index += 3;
-    }
+    decodedString += decodedBytes[0];
+    index += decodedBytes[1];
   }
   return decodedString;
 };
